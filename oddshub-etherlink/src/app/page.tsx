2951d0895310807c377d0026ba7c7eb278989381
Devansh goel <f20220457@pilani.bits-pilani.ac.in>
2025-04-11 14:30:45 +0530

    added chain logo

--- oddshub-etherlink/src/app/page.tsx
+++ oddshub-etherlink/src/app/page.tsx
@@ -3,13 +3,14 @@ import CustomLogo from "@/components/common/CustomIcons";
 import "./styles.scss";
 import BetSection from "@/components/BetSection";
 import Image from "next/image";
+import { ETHERLINK_LOGO } from "@/components/helpers/icons";
 export default function Home() {
   return (
     <main className='Home'>
       <div className='Heading-Section'>
         <span className='Heading'>Use your Insights & Trade on Opinions with ForeCast</span>
         <span className='Sub-Heading'>
-        India’s Primary Prediction Market - built on EtherLink
+        India’s Primary Prediction Market - built on <Image src={ETHERLINK_LOGO} height={22} width={22} alt="MoonBeam" className="ChainLogo"/>EtherLink
         </span>
       </div>
       <BetSection />
